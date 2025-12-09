f31a1be5dece382b6db48d7b8f987470c7e235cf
Alejandro Medel Martínez <amm00331@red.ujaen.es>
2025-12-09 16:30:00 +0100

    Correccion de la funcion leerPartidos()

---
+++ funciones/Todas_las_funciones.py
@@ -0,0 +1,7 @@
+import csv
+
+def leerPartidos() -> list:
+    with open('liga.csv', 'r', encoding="utf-8",) as fCSV:
+        content = list(csv.DictReader(fCSV))
+    fCSV.close()
+    return content
--- funciones/funcionesAlex.py
+++ funciones/funcionesAlex.py
@@ -2,6 +2,6 @@ import csv
 
 def leerPartidos() -> list:
     with open('liga.csv', 'r', encoding="utf-8",) as fCSV:
-        content = list(csv.reader(fCSV))
+        content = list(csv.DictReader(fCSV))
     fCSV.close()
     return content
