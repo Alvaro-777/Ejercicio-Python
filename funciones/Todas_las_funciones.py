4edc5262bd3545e4fdf640da3267821420334e0c
Álvaro Colmenero <acolrod814@g.educaand.es>
2025-12-09 16:43:01 +0100

    Añadir comentario funcion 2

--- funciones/Todas_las_funciones.py
+++ funciones/Todas_las_funciones.py
@@ -16,6 +16,14 @@ def leerPartidos() -> list:
 datosliga = leerPartidos()
 
 def equipos(datosliga) -> set:
+    """
+    Recibe la lista de diccionarios generado a partir de la 
+    función anterior, genera los datos de la clasificación y los imprime por pantalla.
+    
+    :param datosliga: Le pasamos los datos extraidos del csv
+    :return: Retorna los equipos
+    :rtype: set
+    """
     equipos = set()
     for partido in datosliga:
         equipos.add(partido["Team 1"])
