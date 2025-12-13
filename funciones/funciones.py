1fff9cc2bb18d947390dfd1b3c1395946e73fc00
Alvaro-777 <acolrod814@g.educaand.es>
2025-12-13 13:25:59 +0100

    Agrego las funciones que me faltaban (Álvaro Colmenero)
    Added functions to calculate team statistics and display league standings.

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -48,3 +48,69 @@ def clasificacion(datos: list) -> list:
 
 datosliga = leerPartidos()
 impClasificacionLiga(datosliga)
+
+def infoEquipos(datosLiga: list, equipos: set) -> list[tuple]:
+    """
+    Recibe los datos de la liga y el conjunto de equipos.
+    Devuelve una lista de tuplas:
+    (equipo, ganados, empatados, perdidos, puntos)
+    """
+    info = []
+
+    for equipo in equipos:
+        ganados = 0
+        empatados = 0
+        perdidos = 0
+
+        for partido in datosLiga:
+            resultado = quienGana(partido["FT"])
+
+            if partido["Team 1"] == equipo:
+                if resultado == 1:
+                    ganados += 1
+                elif resultado == 0:
+                    empatados += 1
+                else:
+                    perdidos += 1
+
+            elif partido["Team 2"] == equipo:
+                if resultado == -1:
+                    ganados += 1
+                elif resultado == 0:
+                    empatados += 1
+                else:
+                    perdidos += 1
+
+        pts = puntos([ganados, empatados, perdidos])
+        info.append((equipo, ganados, empatados, perdidos, pts))
+
+    return info
+
+
+def puntos(info: list) -> int:
+    """
+    Recibe una lista [ganados, empatados, perdidos]
+    y devuelve los puntos (3-1-0)
+    """
+    ganados = info[0]
+    empatados = info[1]
+    return ganados * 3 + empatados
+
+
+def impClasificacion(liga) -> None:
+    """
+    Genera la clasificación y la muestra por pantalla
+    """
+    conj_equipos = equipos(liga)
+    datos = infoEquipos(liga, conj_equipos)
+
+    # ordenar por puntos (último elemento de la tupla)
+    datos_ordenados = sorted(datos, key=lambda x: x[4], reverse=True)
+
+    print(" Nº | Equipo | PG | PE | PP | Puntos")
+    print("-------------------------------------")
+
+    posicion = 1
+    for equipo, pg, pe, pp, pts in datos_ordenados:
+        print(f"{posicion:2} | {equipo:15} | {pg:2} | {pe:2} | {pp:2} | {pts:3}")
+        posicion += 1
